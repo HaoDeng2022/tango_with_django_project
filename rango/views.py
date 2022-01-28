c5e2dfca2f1a307321616562efd7071cfa814180
Hao Deng <2672726d@student.gla.ac.uk>
2022-01-28 01:15:38 +0000

    second commit

--- rango/views.py
+++ rango/views.py
@@ -3,4 +3,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    return render(request, 'rango/index.html', context=context_dict)
     return HttpResponse("Rangosays hey there partner!")
